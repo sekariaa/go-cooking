1e91f813a94847a9eb78f169ce2b66650fa42494
sekariaa <sekariaa@gmail.com>
2024-02-06 20:08:27 +0700

    first using redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { Provider } from './context/context'
+import store from '../src/states/index'
+import { Provider } from 'react-redux'
 import App from './App'
 import './styles/style.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-	<React.StrictMode>
-		<Provider>
-			<BrowserRouter>
-				<React.StrictMode>
-					<App />
-				</React.StrictMode>
-			</BrowserRouter>
-		</Provider>
-	</React.StrictMode>
+	<Provider store={store}>
+		<BrowserRouter>
+			<React.StrictMode>
+				<App />
+			</React.StrictMode>
+		</BrowserRouter>
+	</Provider>
 )
